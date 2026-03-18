4ca3231f9a82b9f2cc9b059399f1c2af20cc63d3
Ritik kumar <149752140+rk-ritik-raj@users.noreply.github.com>
2026-03-18 13:11:26 +0530

    Update main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -5,12 +5,14 @@ import App from './App.jsx'
 import { BrowserRouter } from 'react-router-dom'
 import { Provider } from 'react-redux'
 import { store } from './redux/store.js'
+import axios from "axios"
+
+axios.defaults.withCredentials = true
 
 createRoot(document.getElementById('root')).render(
   <BrowserRouter>
-  <Provider store={store}>
-    <App />
-  </Provider>
+    <Provider store={store}>
+      <App />
+    </Provider>
   </BrowserRouter>
- 
 )
