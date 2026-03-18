13bf1f76a8203cd24a168b85c67b1e1f3275d9a4
Ritik kumar <149752140+rk-ritik-raj@users.noreply.github.com>
2026-03-18 12:36:13 +0530

    Add new CORS origin for backend

--- backend/index.js
+++ backend/index.js
@@ -21,7 +21,8 @@ const io=new Server(server,{
    cors:{
    
   
- origin: "https://vingo-l7iv.onrender.com",
+ "https://vingo-l7iv.onrender.com",
+"https://vingo-backend-fpoy.onrender.com"
   credentials: true,
   methods: ['POST','GET']
 }
@@ -33,7 +34,8 @@ app.set("io",io)
 
 const port=process.env.PORT || 5000
 app.use(cors({
-    origin:"https://vingo-l7iv.onrender.com",
+    "https://vingo-l7iv.onrender.com",
+   "https://vingo-backend-fpoy.onrender.com",
     credentials:true
 }))
 app.use(express.json())
